cbaeac23466be4fbdad5ff4d7dc70513cdbce4b2
evergreenacorn <rasakjustone01.11@gmail.com>
2021-07-20 21:39:40 +0300

    Добавлены контроллеры.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
 from book_store import create_app, db
 from book_store.models import Author, Book
+from book_store.api import BookViewAPI, AuthorViewAPI
+# from book_store.api import book_list, detail
 
 
 app = create_app()
+app.add_url_rule(
+    '/api/books',
+    methods=['GET'],
+    view_func=BookViewAPI.books_list)
+
+app.add_url_rule(
+    '/api/books/<int:pk>',
+    methods=['GET'],
+    view_func=BookViewAPI.book_detail)
+
+
+app.add_url_rule(
+    '/api/authors',
+    methods=['GET'],
+    view_func=AuthorViewAPI.authors_list)
+
+
+app.add_url_rule(
+    '/api/authors/<int:pk>',
+    methods=['GET'],
+    view_func=AuthorViewAPI.author_detail)
 
 
 @app.shell_context_processor
