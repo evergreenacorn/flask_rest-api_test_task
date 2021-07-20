2baf66626369a972ba83af21960916b9387abd78
evergreenacorn <rasakjustone01.11@gmail.com>
2021-07-20 11:21:05 +0300

    Обновлены зависимости. Добавлен файл настроек проекта.

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
+        'sqlite:///' + os.path.join(BASE_DIR, 'app.sqlite3')
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
