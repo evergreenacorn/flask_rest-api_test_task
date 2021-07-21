7b645cfe551c874d7de5fd68dbd43e195562de38
evergreenacorn <rasakjustone01.11@gmail.com>
2021-07-21 14:22:49 +0300

    Добавлен маршрут для api обновления записи Book по id.

--- app/main.py
+++ app/main.py
@@ -5,30 +5,39 @@ from book_store.api import BookViewAPI, AuthorViewAPI
 
 app = create_app()
 # Book API
+# get all books
 app.add_url_rule(
     '/api/books',
     methods=['GET'],
     view_func=BookViewAPI.books_list)
 
+# get book by id
 app.add_url_rule(
     '/api/books/<int:pk>',
     methods=['GET'],
     view_func=BookViewAPI.book_detail)
 
-
+# create new book
 app.add_url_rule(
     '/api/books',
     methods=['POST'],
     view_func=BookViewAPI.new_book)
 
+# update book
+app.add_url_rule(
+    '/api/books/<int:pk>',
+    methods=['PUT'],
+    view_func=BookViewAPI.update_book)
+
 
 # Author API
+# get all authors
 app.add_url_rule(
     '/api/authors',
     methods=['GET'],
     view_func=AuthorViewAPI.authors_list)
 
-
+# get author by id
 app.add_url_rule(
     '/api/authors/<int:pk>',
     methods=['GET'],
