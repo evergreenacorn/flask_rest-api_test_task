9787623899d7d956ab0d9840ee26bf40c12e3b10
evergreenacorn <rasakjustone01.11@gmail.com>
2021-07-22 21:20:09 +0300

    Обновлен docker-compose файл. Добавлен файл с системными переменными для docker-compose; Обновлен config-файл приложения.

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+from os import getenv
 import os
 
 
@@ -5,15 +6,14 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRES_DB', '').replace(
-    #     'postgres://', 'postgresql://') or \
-    #     'sqlite:///' + os.path.join(BASE_DIR, 'app.sqlite3')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.sqlite3')
-    DEBUG = os.environ.get("DEBUG") or True
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
-        "SQLALCHEMY_TRACK_MODIFICATIONS"
-    ) or False
+    SECRET_KEY = getenv('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
+        'sqlite:///' + os.path.join(BASE_DIR, 'app.sqlite3')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '') or 'sqlite:///' + os.path.join(BASE_DIR, 'app.sqlite3')
+    # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/postgres"
+    DEBUG = getenv("DEBUG") or True
+    SQLALCHEMY_TRACK_MODIFICATIONS = getenv("SQLALCHEMY_TRACK_MODIFICATIONS") or False
 
 
 class TestConfig:
