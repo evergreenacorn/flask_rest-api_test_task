b440d7eaf1248dc9f6b3f162283f97b663acad65
evergreenacorn <rasakjustone01.11@gmail.com>
2021-07-22 20:11:45 +0300

    Добавлены все тесты API.

--- app/tests/test_author_api.py
+++ app/tests/test_author_api.py
@@ -1,4 +1,7 @@
-from tests.test_app import Book, Author, testing_client, init_database
+from tests.test_app import (
+    Book, Author,
+    testing_client, init_database,
+)
 
 
 def test_author_list(testing_client, init_database):
@@ -7,13 +10,64 @@ def test_author_list(testing_client, init_database):
     assert len(response.json['authors']) == 5
 
 
-def test_author_detail(testing_client, init_database): ...
+def test_author_detail_first(testing_client, init_database):
+    response = testing_client.get("/api/authors/1")
+    try:
+        first_author = Author.query.get(1)
+        assert response.status_code == 200
+        assert response.json['author']['fio'] == first_author.fio
+    except Exception as e:
+        raise e
 
 
-def test_new_author(testing_client, init_database): ...
+def test_new_author(testing_client, init_database):
+    mimetype = "application/json"
+    headers = {'Content-Type': mimetype, 'Accept': mimetype}
+    data = {
+        "fio": "ПЕТРОВ ИВАН КУЗЬМИЧ"
+    }
+    response = testing_client.post(
+        "/api/authors",
+        json=data,
+        headers=headers)
+    assert response.status_code == 200
+    assert response.json['message'] == "Created new author"
 
 
-def test_update_author(testing_client, init_database): ...
+def test_update_author(testing_client, init_database):
+    updated_author = Author.query.get(1)
+    data = {"author": {
+        "id": updated_author.id,
+        "fio": updated_author.fio,
+        "book_id": updated_author.book_id,
+        "created_at": updated_author.created_at,
+        "updated_at": updated_author.updated_at
+    }}
+    data["fio"] = "ИВАНОВ СТЕПАН ИГНАТЬЕВИЧ"
+    mimetype = "application/json"
+    headers = {'Content-Type': mimetype, 'Accept': mimetype}
+    response = testing_client.put(
+        "/api/authors/1",
+        json=data,
+        headers=headers)
+    assert response.status_code == 200
+    assert response.json['message'] == f"Updated author [id: {updated_author.id}]"
+    assert response.json['author']['fio'] == "ИВАНОВ СТЕПАН ИГНАТЬЕВИЧ"
 
 
-def test_delete_author(testing_client, init_database): ...
+def test_delete_author(testing_client, init_database):
+    deleted_author = Author(fio="ДЯДЯ СТЕПА СВЕТОФОР").create()
+    data = {"author": {
+        "id": deleted_author.id,
+        "fio": deleted_author.fio,
+        "book_id": deleted_author.book_id,
+        "created_at": deleted_author.created_at,
+        "updated_at": deleted_author.updated_at
+    }}
+    mimetype = "application/json"
+    headers = {'Content-Type': mimetype, 'Accept': mimetype}
+    response = testing_client.delete(
+        "/api/authors/1",
+        json=data,
+        headers=headers)
+    assert response.status_code == 204
--- app/tests/test_book_api.py
+++ app/tests/test_book_api.py
@@ -1,4 +1,7 @@
-from tests.test_app import Book, Author, testing_client, init_database
+from tests.test_app import (
+    Book, Author,
+    testing_client, init_database,
+)
 
 
 def test_books_list_w_authors(testing_client, init_database):
@@ -25,13 +28,72 @@ def test_book_detail_first_list(testing_client, init_database):
 
 
 def test_new_book(testing_client, init_database):
-    pass
+    mimetype = "application/json"
+    headers = {'Content-Type': mimetype, 'Accept': mimetype}
+    data = {
+        "name": "АЛГОРИТМЫ И СТРУКТУРЫ ДАННЫХ",
+        "pages_num": 992,
+    }
+    response = testing_client.post(
+        "/api/books",
+        json=data,
+        headers=headers)
+    assert response.status_code == 200
+    assert response.json['message'] == "Created new book"
 
 
-def test_new_book_w_authors(testing_client, init_database): ...
+def test_new_book_w_authors(testing_client, init_database):
+    mimetype = "application/json"
+    headers = {'Content-Type': mimetype, 'Accept': mimetype}
+    data = {
+        "name": "АЛГОРИТМЫ И СТРУКТУРЫ ДАННЫХ. ЧАСТЬ 2",
+        "pages_num": 997,
+        "authors": [3]
+    }
+    response = testing_client.post(
+        "/api/books",
+        json=data,
+        headers=headers)
+    assert response.status_code == 200
+    assert response.json['message'] == "Created new book"
 
 
-def test_update_book(testing_client, init_database): ...
+def test_update_book(testing_client, init_database):
+    updated_book = Book.query.get(1)
+    data = {"book": {
+        "id": updated_book.id,
+        "name": updated_book.name,
+        "pages_num": updated_book.pages_num,
+        "created_at": updated_book.created_at,
+        "updated_at": updated_book.updated_at,
+        "authors": updated_book.authors
+    }}
+    data["name"] = "ЛОЛКЕКЧЕБУРЕК"
+    mimetype = "application/json"
+    headers = {'Content-Type': mimetype, 'Accept': mimetype}
+    response = testing_client.put(
+        "/api/books/1",
+        json=data,
+        headers=headers)
+    assert response.status_code == 200
+    assert response.json['message'] == f"Updated book [id: {updated_book.id}]"
+    assert response.json['book']['name'] == "ЛОЛКЕКЧЕБУРЕК"
 
 
-def test_delete_book(testing_client, init_database): ...
+def test_delete_book(testing_client, init_database):
+    deleted_book = Book(name="УДАЛИ МЕНЯ", pages_num=111).create()
+    data = {"book": {
+        "id": deleted_book.id,
+        "name": deleted_book.name,
+        "pages_num": deleted_book.pages_num,
+        "created_at": deleted_book.created_at,
+        "updated_at": deleted_book.updated_at,
+        "authors": deleted_book.authors
+    }}
+    mimetype = "application/json"
+    headers = {'Content-Type': mimetype, 'Accept': mimetype}
+    response = testing_client.delete(
+        "/api/books/1",
+        json=data,
+        headers=headers)
+    assert response.status_code == 204
