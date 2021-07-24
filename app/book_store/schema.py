32bfdad2769ea4d1abc5bd8061428eac66ff99b7
evergreenacorn <rasakjustone01.11@gmail.com>
2021-07-24 14:50:24 +0300

    Обновлена схема, добавлена возможность вывода вложенного списка записей авторов книги.

--- app/book_store/schema.py
+++ app/book_store/schema.py
@@ -1,12 +1,12 @@
 from book_store.models import Book, Author, db
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
-# from book_store import ma
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field, fields
 
 
 class AuthorSchema(SQLAlchemyAutoSchema):
     """
         Класс схемы(аналог сериализатора drf) модели Author
     """
+
     class Meta:
         model = Author
         include_fk = True
@@ -17,6 +17,8 @@ class BookSchema(SQLAlchemyAutoSchema):
     """
         Класс схемы(аналог сериализатора drf) модели Book
     """
+    authors = fields.Nested(AuthorSchema(many=True, exclude=('book_id',)))
+
     class Meta:
         model = Book
         include_relationships = True
